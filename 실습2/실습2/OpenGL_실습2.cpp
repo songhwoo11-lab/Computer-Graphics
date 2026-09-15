53cc40886a350e7fe572bb9fa96fe16df6a899b7
song04 <songhwoo11@gmail.com>
2026-09-15 21:04:15 +0900

    Refactor rectangle selection and rendering logic

--- 실습2/실습2/OpenGL_실습2.cpp
+++ 실습2/실습2/OpenGL_실습2.cpp
@@ -1,4 +1,4 @@
-﻿#include<iostream>
+#include<iostream>
 #include<vector>
 #include<random>
 #include<gl/glew.h>
@@ -21,11 +21,54 @@ struct Rect {
 };
 
 std::vector<Color> bgc(4);
-std::vector<Rect> select;
+std::vector<std::pair<Rect, int>> select;
 std::random_device rd;
 std::mt19937 dre{ rd() };
 std::uniform_int_distribution uid{ 0, 255 };
 int selm = -1;
+
+int cntRect(int where)
+{
+	int cnt = 0;
+	for (const auto& sel : select) {
+		if (sel.second == where) cnt++;
+	}
+	return cnt;
+}
+bool upCheck(const std::pair<Rect, int>& sel, double degree) {
+	double toSize = sel.first.size + degree;
+	switch (sel.second) {
+	case 1:
+		if (sel.first.x - toSize <= 0 or sel.first.x - toSize >= 1.0f or
+			sel.first.y - toSize <= 0 or sel.first.y - toSize >= 1.0f or
+			sel.first.x + toSize <= 0 or sel.first.x + toSize >= 1.0f or
+			sel.first.y + toSize <= 0 or sel.first.y + toSize >= 1.0f
+			) return false;
+		break;
+	case 2:
+		if (sel.first.x - toSize >= 0 or sel.first.x - toSize <= -1.0f or
+			sel.first.y - toSize <= 0 or sel.first.y - toSize >= 1.0f or
+			sel.first.x + toSize >= 0 or sel.first.x + toSize <=  -1.0f or
+			sel.first.y + toSize <= 0 or sel.first.y + toSize >= 1.0f
+			) return false;
+		break;
+	case 3:
+		if (sel.first.x - toSize >= 0 or sel.first.x - toSize <= -1.0f or
+			sel.first.y - toSize >= 0 or sel.first.y - toSize <=  -1.0f or
+			sel.first.x + toSize >= 0 or sel.first.x + toSize <=  -1.0f or
+			sel.first.y + toSize >= 0 or sel.first.y + toSize <=  -1.0f
+			) return false;
+		break;
+	case 4:
+		if (sel.first.x - toSize <= 0 or sel.first.x - toSize >= 1.0f or
+			sel.first.y - toSize >= 0 or sel.first.y - toSize <= -1.0f or
+			sel.first.x + toSize <= 0 or sel.first.x + toSize >= 1.0f or
+			sel.first.y + toSize >= 0 or sel.first.y + toSize <= -1.0f
+			) return false;
+		break;
+	}
+	return true;
+}
 
 bool timer = false;
 double dtime = 1;
@@ -33,7 +76,7 @@ void OpenGL::startPractice()
 {
 	for (auto& col : bgc)
 		col = { uid(dre) / 255.f, uid(dre) / 255.f, uid(dre) / 255.f };
-	
+
 }
 void OpenGL::mainloop() {
 	//--- 메인 루프
@@ -58,26 +101,26 @@ void OpenGL::rendering() {
 
 	// 배경 그리기
 	for (int i = 0; i < 2; ++i) {
-		for (int j= 0; j < 2; ++j) {
+		for (int j = 0; j < 2; ++j) {
 			glColor3f(bgc[2 * i + j].r, bgc[2 * i + j].g, bgc[2 * i + j].b);
-			glRectf(i- 1, j - 1, i, j);
+			glRectf(i - 1, j - 1, i, j);
 		}
 	}
 
 	// 생성된 사각형 그리기
 	for (const auto& sel : select) {
-		glColor3f(sel.color.r, sel.color.g, sel.color.b);
-		glRectf(sel.x - sel.size, sel.y - sel.size,
-			sel.x + sel.size, sel.y + sel.size);
+		glColor3f(sel.first.color.r, sel.first.color.g, sel.first.color.b);
+		glRectf(sel.first.x - sel.first.size, sel.first.y - sel.first.size,
+			sel.first.x + sel.first.size, sel.first.y + sel.first.size);
 
 	}
 	// 선택된 사각형 그리기
-	if(selm >= 0){
-		glColor3f(0.0f,0.0f,0.0f);
+	if (selm >= 0) {
+		glColor3f(0.0f, 0.0f, 0.0f);
 		glLineWidth(3.0f);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		glRectf(select[selm].x - select[selm].size, select[selm].y - select[selm].size,
-			select[selm].x + select[selm].size, select[selm].y + select[selm].size);
+		glRectf(select[selm].first.x - select[selm].first.size, select[selm].first.y - select[selm].first.size,
+			select[selm].first.x + select[selm].first.size, select[selm].first.y + select[selm].first.size);
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	}
 
@@ -117,17 +160,17 @@ void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 	int width, height;
 	switch (button) {
 	case GLFW_MOUSE_BUTTON_LEFT:
-		if(action == GLFW_PRESS)
+		if (action == GLFW_PRESS)
 		{
 			std::cout << "mouse left click\n";
 			glfwGetCursorPos(window, &x, &y);
 			glfwGetWindowSize(window, &width, &height);
 			x = x - width / 2.;
 			y = height / 2. - y;
-			for (auto itr = select.begin(); itr != select.end(); ++itr) {
-				if ((width / 2.) * (itr->x - itr->size) <= x and x <= (width / 2.)*(itr->x + itr->size) and
-					(height / 2.) * (itr->y - itr->size) <= y and y <= (height / 2.) * (itr->y + itr->size)) {
-					selm = (itr - select.begin());
+			for (int i = select.size() - 1; i > -1; --i) {
+				if ((width / 2.) * (select[i].first.x - select[i].first.size) <= x and x <= (width / 2.) * (select[i].first.x + select[i].first.size) and
+					(height / 2.) * (select[i].first.y - select[i].first.size) <= y and y <= (height / 2.) * (select[i].first.y + select[i].first.size)) {
+					selm = i;
 					break;
 				}
 				else selm = -1;
@@ -149,55 +192,49 @@ void CharCallback(GLFWwindow* window, unsigned int codepoint)
 {
 	std::cout << codepoint << " == " << (char)codepoint << '\n';
 
+	std::uniform_int_distribution ran{ 10, 80 };
+	double degree = pow(2, 5);
 	switch (codepoint) {
 	case 'q':
 		glfwSetWindowShouldClose(window, true);
 		break;
 	case '1':
-		if (select.size() < 5) {
-		std::uniform_int_distribution size{ 10, 50 };
-		GLfloat sz = size(dre) / 100.;
-		Rect rect{ 0.5f, 0.5f, sz, {uid(dre) / 255.,uid(dre) / 255.,uid(dre) / 255.} };
-		select.push_back(rect);
-	}
+		if (cntRect(1) < 5) {
+			Rect rect{ ran(dre)/100., ran(dre) / 100.,0.1f, {uid(dre) / 255.,uid(dre) / 255.,uid(dre) / 255.}};
+			select.push_back({ rect , 1});
+		}
 		break;
 	case '2':
-		if (select.size() < 5) {
-		std::uniform_int_distribution size{ 10, 50 };
-		GLfloat sz = size(dre) / 100.;
-		Rect rect{ -0.5f, 0.5f, sz, {uid(dre) / 255.,uid(dre) / 255.,uid(dre) / 255.} };
-		select.push_back(rect);
-	}
+		if (cntRect(2) < 5) {
+			Rect rect{ -ran(dre) / 100., ran(dre) / 100., 0.1f, {uid(dre) / 255.,uid(dre) / 255.,uid(dre) / 255.} };
+			select.push_back({ rect, 2 });
+		}
 		break;
 	case '3':
-	if(select.size() < 5){
-		std::uniform_int_distribution size{ 10, 50 };
-		GLfloat sz = size(dre) / 100.;
-		Rect rect{ -0.5f, -0.5f, sz, {uid(dre) / 255.,uid(dre) / 255.,uid(dre) / 255.} };
-		select.push_back(rect);
-	}
+		if (cntRect(3) < 5) {
+			Rect rect{ -ran(dre) / 100., -ran(dre) / 100.,0.1f,  {uid(dre) / 255.,uid(dre) / 255.,uid(dre) / 255.} };
+			select.push_back({ rect, 3 });
+		}
 		break;
 	case '4':
-		if (select.size() < 5) {
-		std::uniform_int_distribution size{ 10, 50 };
-		GLfloat sz = size(dre) / 100.;
-		Rect rect{ 0.5f, -0.5f, sz, {uid(dre) / 255.f, uid(dre) / 255.f, uid(dre) / 255.f} };
-		select.push_back(rect);
-	}
+		if (cntRect(4) < 5) {
+			Rect rect{ ran(dre) / 100., -ran(dre) / 100.,0.1f, {uid(dre) / 255.f, uid(dre) / 255.f, uid(dre) / 255.f} };
+			select.push_back({ rect, 4 });
+		}
 		break;
 	case '+': // 크기 늘리기
 		if (selm >= 0) {
-			if (select[selm].size < 0.49f) select[selm] .size += 1 / pow(2, 7);
+			if (upCheck(select[selm], 1 / degree)) select[selm].first.size += 1 / degree;
 		}
 		break;
 	case '-': // 크기 줄이기
 		if (selm >= 0) {
-			if (select[selm].size > 0.1f) select[selm].size -= 1 / pow(2, 7);
+			if (select[selm].first.size > 0.05f) select[selm].first.size -= 1 / degree;
 		}
 		break;
 	case 'c': // 색 변경
 		if (selm >= 0) {
-			select[selm].color = { uid(dre) / 255.f, uid(dre) / 255.f,uid(dre) / 255.f };
+			select[selm].first.color = { uid(dre) / 255.f, uid(dre) / 255.f,uid(dre) / 255.f };
 		}
 		break;
 	case 'r':
