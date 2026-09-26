c79a5e9aedfe7a2114259b4bf3efac8ef81fd6f2
song04 <songhwoo11@gmail.com>
2026-09-26 14:10:25 +0900

    Add rotation uniform to vertex shader

--- 실습9/실습9/vertex.glsl
+++ 실습9/실습9/vertex.glsl
@@ -1,5 +1,6 @@
 #version 330 core
 
+uniform mat4 rotate;
 layout (location = 0) in vec3 vPos;
 layout (location = 1) in vec3 vColor;
 
@@ -7,6 +8,6 @@ out vec3 outColor;
 
 void main()
 {
-	gl_Position = vec4(vPos, 1.0);
+	gl_Position = rotate * vec4(vPos, 1.0);
 	outColor = vColor;
 }
